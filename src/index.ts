8f1822dac88aedda574e7982555436695cd0865b
Mohammad Bekran <bhshtmhmd1@gmail.com>
2025-06-06 21:59:54 +0330

    feat(type): add index.ts file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { default as Button } from "./features/button/components";
+export type { IButtonProps } from "./features/button/core/types";
+
+export { default as Input } from "./features/input/components";
+export type { IInputProps } from "./features/input/core/types";
+
+export { default as Modal } from "./features/modal/components";
+export type { IModalClasses } from "./features/modal/core/types";
+
+export { default as Calendar } from "./features/calendar/components";
+export type { ICalendarProps } from "./features/calendar/core/types";
