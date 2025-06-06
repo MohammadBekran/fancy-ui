df71e9e74d93826c0757129887b0d390c31bdfdb
Mohammad Bekran <bhshtmhmd1@gmail.com>
2025-06-06 21:54:42 +0330

    chore: bump version to 0.1.1 in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path, { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "FancyUI",
+      fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: [
+        { format: "cjs", dir: "dist/cjs" },
+        { format: "es", dir: "dist/esm" },
+      ],
+    },
+    sourcemap: true, // Helps debug build
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
